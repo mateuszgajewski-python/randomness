bbf3db268131924a8b03fce28f377cfbcc58c4ba
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-02-01 15:22:51 +0100

    21.02.01

---
+++ shuffle.py
@@ -0,0 +1,15 @@
+#shuffle - zmiana kolejności elemntow listy
+
+import random
+
+cardList = ["9", "9", "9", "9",
+            "10", "10", "10", "10",
+            "Jack", "Jack", "Jack", "Jack",
+            "Queen", "Queen", "Queen", "Queen",
+            "King", "King", "King", "King",
+            "Ace", "Ace", "Ace", "Ace",
+            "Joker", "Joker"]
+
+random.shuffle(cardList)
+
+print(cardList)
